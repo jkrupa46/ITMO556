403961b7f906bc0f6ec57afb4fd1fbaf0049ce1c
Jacob <jkrupa46@gmail.com>
2020-03-19 14:42:22 -0500

    added test phone number

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
 Use users;
 
-INSERT INTO customer (id, email, create_date, password, last_name, first_name, phone) Values (0, "joedoe@gmail.com", CURRENT_TIMESTAMP, "password123", "Doe", "Joe");
+INSERT INTO customer (id, email, create_date, password, last_name, first_name, phone) Values (0, "joedoe@gmail.com", CURRENT_TIMESTAMP, "password123", "Doe", "Joe", "708-708-7080");
