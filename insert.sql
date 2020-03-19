c7c3f36194ce801059a7a6fbfd40dbaf99c8c9d9
Jacob <jkrupa46@gmail.com>
2020-03-19 14:31:20 -0500

    changed name of database

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
-Use comments;
+Use users;
 
 INSERT INTO customer (id, email, create_date, password, last_name, first_name, phone) Values (0, "joedoe@gmail.com", CURRENT_TIMESTAMP, "password123", "Doe", "Joe");
