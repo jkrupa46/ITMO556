c45f4e947f25b00fe8ba7af581da9b74bc0ce492
Jacob <jkrupa46@gmail.com>
2020-03-19 14:13:57 -0500

    added customer database schema

--- create.sql
+++ create.sql
@@ -1,11 +1,14 @@
 CREATE DATABASE IF NOT EXISTS comments; 
 
-USE comments;
+USE user;
 
-CREATE TABLE comment 
-(
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
+CREATE TABLE customer (
+  id  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email     VARCHAR(255)    NOT NULL,
+  create_date   DATETIME    NOT NULL,
+  password      VARCHAR(255)   NOT NULL,
+  last_name     VARCHAR(255),
+  first_name    VARCHAR(255),
+  zip           CHAR(10),
+  phone         VARCHAR(25),
 );
