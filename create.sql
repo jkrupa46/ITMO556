46191996b99d88f0d1d02ccaa84b1ba8cdc10e4e
Jacob <jkrupa46@gmail.com>
2020-03-20 00:37:26 -0500

    Update create.sql

--- create.sql
+++ create.sql
@@ -4,7 +4,7 @@ USE users;
 
 CREATE TABLE customer (
   id  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email     VARCHAR(255)    NOT NULL,
+  username     VARCHAR(255)    NOT NULL,
   create_date   DATETIME    NOT NULL,
   password      VARCHAR(255)   NOT NULL,
   last_name     VARCHAR(255),
