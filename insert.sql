d7d3cb7fef580d5a2f152fa4df368d35d5b7f9c3
Jacob <jkrupa46@gmail.com>
2020-03-22 16:46:54 -0500

    changed password for test user

--- insert.sql
+++ insert.sql
@@ -1,3 +1,3 @@
 Use users;
 
-INSERT INTO customer (id, username, create_date, password, last_name, first_name, phone) Values (0, "joedoe@gmail.com", CURRENT_TIMESTAMP, "password123", "Doe", "Joe", "708-708-7080");
+INSERT INTO customer (id, username, create_date, password, last_name, first_name, phone) Values (0, "joedoe@gmail.com", CURRENT_TIMESTAMP, "098f6bcd4621d373cade4e832627b4f6", "Doe", "Joe", "708-708-7080");
